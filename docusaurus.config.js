65a6f45732e994f07e055809e45d39c49c4c50e8
Ahmed Bargady <bargadyahmed2000@gmail.com>
2023-02-05 08:08:00 +0100

    dark mode theming

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,83 +1,91 @@
 // @ts-check
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
-const math = require('remark-math');
-const katex = require('rehype-katex');
-
+const math = require("remark-math");
+const katex = require("rehype-katex");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Sentiment',
-  tagline: 'The Universal Credit Protocol',
-  url: 'https://sentiment.xyz',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'favicon.ico',
-  organizationName: 'sentimentxyz',
-  projectName: 'docs',
+  title: "Sentiment",
+  tagline: "The Universal Credit Protocol",
+  url: "https://sentiment.xyz",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "favicon.ico",
+  organizationName: "sentimentxyz",
+  projectName: "docs",
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          routeBasePath: '/',
-          sidebarPath: require.resolve('./sidebars.js'),
-          editUrl: 'https://github.com/sentimentxyz/docs',
+          routeBasePath: "/",
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/sentimentxyz/docs",
           remarkPlugins: [math],
           rehypePlugins: [katex],
         },
-        blog: false
+        blog: false,
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
       }),
     ],
   ],
   stylesheets: [
     {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
-      type: 'text/css',
+      href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
+      type: "text/css",
       integrity:
-        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
-      crossorigin: 'anonymous',
+        "sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
+      crossorigin: "anonymous",
     },
   ],
 
   themeConfig: {
-      hideableSidebar: true,
-      autoCollapseSidebarCategories: true,
-      navbar: {
-        title: 'Sentiment',
-        logo: {
-          alt: 'Sentiment',
-          src: 'logo.png',
-        },
-        items: [
-          {
-            label: 'Twitter',
-            href: 'https://twitter.com/sentimentxyz',
-            position: 'right'
-          },
-          {
-            label: 'GitHub',
-            href: 'https://github.com/sentimentxyz',
-            position: 'right',
-          },
-        ]
-      },
-      footer: {
-        style: 'dark',
-        copyright: `Copyright © ${new Date().getFullYear()} Sentiment.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-        additionalLanguages: ['solidity'],
-      },
+    autoCollapseSidebarCategories: true,
+    algolia: {
+      apiKey: "4b42612579d6552c7052026237368745",
+      appId: "COO2IEW09E",
+      indexName: "prod_index",
     },
+    navbar: {
+      logo: {
+        alt: "Sentiment",
+        src: "logo_long.png",
+      },
+      items: [
+        {
+          label: "Twitter",
+          href: "https://twitter.com/sentimentxyz",
+          position: "right",
+        },
+        {
+          label: "GitHub",
+          href: "https://github.com/sentimentxyz",
+          position: "right",
+        },
+        {
+          type: "search",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      copyright: `Copyright © ${new Date().getFullYear()} Sentiment.`,
+    },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+      additionalLanguages: ["solidity"],
+    },
+  },
 };
-
 
 module.exports = config;
